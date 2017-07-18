57a34baff684c5964a1cdfafa1d8049f933471c7
Dillon Strichman <dstrichman@gmail.com>
2017-07-18 03:46:29 -0400

    express server and p5.js canvas

---
+++ basement/sketch.js
@@ -0,0 +1,20 @@
+var canvas;
+var mic;
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index', '-1');
+  mic = new p5.AudioIn();
+  mic.start();
+}
+
+function draw() {
+  background(51);
+  var vol = mic.getLevel();
+  ellipse(width/2, height/2, vol*width);
+
+}
--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Starting server . . .");
+
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000, listening);
+
+app.use(express.static('basement'));
+
+function listening() {
+  console.log("Starting server . . .");
+
+}
